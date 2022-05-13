0a6c6507221657160ddf8c4f31ebbd14ed9e7583
Emily Harrison <ekharrison.art@gmail.com>
2022-05-13 13:27:49 -0600

    thymeleaf excercises mostly done

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,41 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
+    @GetMapping
+    public String displayAllEvents(Model model){
+//        model.addAttribute("events", events);
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Code", "Coding");
+        events.put("Coder", "Coded");
+        events.put("Coding", "Codes");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+        //redirect to root path
+    }
+
+}
